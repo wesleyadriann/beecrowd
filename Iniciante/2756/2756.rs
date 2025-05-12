a57cd851d677f9f927d784787098c3876d2b2d22
Wesley Adriann <wesleyadriann@gmail.com>
2025-05-12 15:28:21 -0300

    adds 2756
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ Iniciante/2756/2756.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("       A");
+    println!("      B B");
+    println!("     C   C");
+    println!("    D     D");
+    println!("   E       E");
+    println!("    D     D");
+    println!("     C   C");
+    println!("      B B");
+    println!("       A");
+}
