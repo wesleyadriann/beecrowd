8053f6eb5aae1ea60161c6874a3a7c64787a3609
Wesley Adriann <wesleyadriann@gmail.com>
2025-07-25 18:02:51 -0300

    adds 1002 1003 1004 python
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ Iniciante/1002/1002.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+PI = 3.14159
+
+r = float(input())
+result = PI * (r * r);
+
+print(f"A={result:.4f}");
---
+++ Iniciante/1003/1003.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+result = a + b
+
+print(f"SOMA = {result}")
