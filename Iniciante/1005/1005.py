4aaa9104a29be436bc0e231e26a88ae7465d76ca
Wesley Adriann <wesleyadriann@gmail.com>
2025-07-28 19:29:21 -0300

    adds 1005 1006 python
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ Iniciante/1005/1005.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+a = float(input())
+b = float(input())
+
+r = ((a * 3.5) + (b * 7.5)) / 11
+
+print(f"MEDIA = {r:.5f}");
---
+++ Iniciante/1006/1006.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+r = ((a * 2) + (b * 3) + (c * 5)) / 10
+
+print(f"MEDIA = {r:.1f}")
