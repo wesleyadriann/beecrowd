39e579efc44d4491275e2d6ad5bb466edcbc4c6a
Wesley Adriann <wesleyadriann@gmail.com>
2025-12-09 18:20:11 -0300

    adds 1002 1003 1004 1005 in lua
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ Iniciante/1002/1002.lua
@@ -0,0 +1,7 @@
+PI = 3.14159
+
+local r = io.read("*n")
+
+local result = PI * (r ^ 2)
+
+print(string.format("A=%.4f", result))
---
+++ Iniciante/1005/1005.lua
@@ -0,0 +1,6 @@
+local a = io.read("*n")
+local b = io.read("*n")
+
+local result = ((a * 3.5) + (b * 7.5)) / 11
+
+print(string.format("MEDIA = %.5f", result))
