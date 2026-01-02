e4aec87c2f54987611286b1793369b88e5ca80ee
Wesley Adriann <wesleyadriann@gmail.com>
2026-01-02 14:59:40 -0300

    adds 2748
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ Iniciante/2748/2748.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("---------------------------------------\n" +
+        "|        Roberto                      |\n" +
+        "|                                     |\n" +
+        "|        5786                         |\n" +
+        "|                                     |\n" +
+        "|        UNIFEI                       |\n" +
+        "---------------------------------------");
+  }
+}
